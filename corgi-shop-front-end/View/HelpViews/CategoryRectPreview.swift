bd647016f09f42ef011f26515d7b5c83e238fb68
mausio <robin.schwenzfeier@icloud.com>
2023-12-10 14:04:25 +0100

    category detail view

--- corgi-shop-front-end/View/HelpViews/CategoryRectPreview.swift
+++ corgi-shop-front-end/View/HelpViews/CategoryRectPreview.swift
@@ -53,8 +53,73 @@ struct CategoryRectPreview: View {
     }
 }
 
-#Preview {
+#Preview("Category") {
     var category = Category(name: "Kissen", imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQacf4n1UIVbzFSiIDYZHnTSqHBxuT0BZuL3zgOJ5sgwVFGjADsd49Eqe0WYUkDCQ04MA&usqp=CAU")
     
     return CategoryRectPreview(category: category)
+}
+
+struct ItemRectPreview: View {
+    var item: Item
+    
+    let recWidth: CGFloat = 145
+    let recHeight: CGFloat = 160
+    
+    
+    var body: some View {
+        Rectangle()
+            .frame(width: recWidth, height: recHeight)
+            .overlay(){
+                ZStack{
+                    LinearGradient(colors: [Color("accentColorFrom"), Color("accentColorTo")], startPoint: .bottomTrailing, endPoint: .top)
+                    
+                    MerchCategoryPreviewImage(urlString: item.imageUrl, recWidth: recWidth, recHeight: recHeight)
+                        .foregroundStyle(Color("strongBlack"))
+                        
+                
+                    VStack(alignment: .leading){
+                        HStack{
+                            if let name = item.name{
+                                Text(name)
+                                    .padding(.top, 15)
+                                    .padding(.leading, 15)
+                                    .padding(.trailing, 5)
+                                    .padding(.bottom, 5)
+                                    .foregroundStyle(Color("lightColor"))
+                                    .background(Color("darkColor"))
+                                    .cornerRadius(10)
+                                    .offset(x: -10, y: -10)
+                                
+                                Spacer()
+                            }
+                        }
+                            Spacer()
+                            HStack{
+                                    if let price = item.price {
+                                        Spacer()
+                                        
+                                        Text("\(price)€")
+                                            .padding(.top, 5)
+                                            .padding(.leading, 5)
+                                            .padding(.trailing, 20)
+                                            .padding(.bottom, 20)
+                                            .foregroundStyle(Color("lightColor"))
+                                            .background(Color("darkColor"))
+                                            .cornerRadius(10)
+                                            .offset(x: 15, y: 15)
+                                    }
+                                }
+                        }
+                }
+            }
+            .cornerRadius(10)
+            .shadow(radius: 10)
+            
+    }
+}
+
+#Preview("Item") {
+    var item = Item(id: 1, name: "Item 1", price: 10, isFavorite: true, description: "This is item 1.", imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQacf4n1UIVbzFSiIDYZHnTSqHBxuT0BZuL3zgOJ5sgwVFGjADsd49Eqe0WYUkDCQ04MA&usqp=CAU")
+    
+    return ItemRectPreview(item: item)
 }
---
+++ corgi-shop-front-end/View/HelpViews/ItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemView.swift
+//  corgi-shop-front-end
+//
+//  Created by Robin Schwenzfeier on 29.11.23.
+//
+
+import SwiftUI
+
+struct ItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemView()
+}
